fc8e831a8c6ab4c9e15726f7c2a2e88198c0a4c9
Comma Device <device@comma.ai>
2019-07-02 16:28:00 -0400

    force mph

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -235,7 +235,8 @@ def state_control(frame, rcv_frame, plan, path_plan, CS, CP, state, events, v_cr
       extra_text = ""
       if e == "belowSteerSpeed":
         if is_metric:
-          extra_text = str(int(round(CP.minSteerSpeed * CV.MS_TO_KPH))) + " kph"
+          extra_text = str(int(round(CP.minSteerSpeed * CV.MS_TO_MPH))) + " 🤖"
+          #extra_text = str(int(round(CP.minSteerSpeed * CV.MS_TO_KPH))) + " kph"
         else:
           extra_text = str(int(round(CP.minSteerSpeed * CV.MS_TO_MPH))) + " mph"
       AM.add(frame, e, enabled, extra_text_2=extra_text)
