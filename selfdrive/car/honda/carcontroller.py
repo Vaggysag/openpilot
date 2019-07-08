4b3fa5de058296c63b77fb4f95029adcfbb3cb19
Comma Device <device@comma.ai>
2019-07-08 18:35:39 -0400

    try accel

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -10,8 +10,8 @@ from common.params import Params
 
 # Accel limits
 ACCEL_HYST_GAP = 0.02 # don't change accel command for small oscilalitons within this value
-ACCEL_MAX = 200.
-ACCEL_MIN = -400.
+ACCEL_MAX = 600.
+ACCEL_MIN = -800.
 ACCEL_SCALE = max(ACCEL_MAX, -ACCEL_MIN)
 
 def accel_hysteresis(accel, accel_steady, enabled):
