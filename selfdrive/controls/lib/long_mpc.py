9260f9ae17e99cfccf0d2cde369db803e6394230
Vaggysag <48258491+Vaggysag@users.noreply.github.com>
2019-07-17 20:24:13 -0400

    Testing get_cost y changes

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -145,7 +145,7 @@ class LongitudinalMpc(object):
 
   def get_cost(self, TR):
     x = [.9, 1.8, 2.7, 3.6]
-    y = [2.5, 2.0, 1.8, 1.55]
+    y = [3.5, 3.0, 2.5, 2.0]
     if self.x_lead is not None and self.v_ego is not None and self.v_ego != 0:
       real_TR = self.x_lead / float(self.v_ego)  # switched to cost generation using actual distance from lead car; should be safer
       if abs(real_TR - TR) >= .25:  # use real TR if diff is greater than x safety threshold
